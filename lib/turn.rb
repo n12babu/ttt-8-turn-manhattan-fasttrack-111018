2af3a8907c4513d12f974d81f556269cee8ea055
Nazrul Babu <n12babu@siena.edu>
2018-11-11 19:34:38 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,25 @@
+board = [" "," "," "," "," "," "," "," "," "]
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move? (board, index);
+index.between?(0,8)&& board[index]==" "
+end
+
+
+
+
+def input_to_index (input)
+  input = input.to_i-1
+
+end
+
+def move (board, index, value= "X")
+  board[index]=value
+
+end
